936e4fd772cb4a05a7965add572e526f25958872
Snehasrg <sghongade642@gmail.com>
2023-06-15 19:04:39 +0530

    [sneha]Added program for OopsConcept

---
+++ Inheritance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class Inheritance
+    {
+        public string brand = "Hyundai";
+        public void honk()
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+    class Car : Inheritance
+    {
+        public string modelName = "Santro";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,10 +4,26 @@
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Welcome To Oops Concepts Problems");
+            Console.WriteLine("Select any one option:- \n1.Object and Class Example." +
+                              "\n2.Inheritance Example.");
 
-            ObjectClass obj = new ObjectClass();
-            Console.WriteLine(obj.Name);
-            Console.WriteLine(obj.Colour);
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    ObjectClass obj = new ObjectClass();
+                    Console.WriteLine(obj.Name);
+                    Console.WriteLine(obj.Colour);
+                    break;
+                case 2:
+                    Car myCar = new Car();
+                    myCar.honk();
+                    Console.WriteLine(myCar.brand + " " + myCar.modelName);
+                    break;
+            }                         
+        
         }
     }
 }
