7be4cf28e1f3d379442c36cd31cbd0f64215e584
Snehasrg <sghongade642@gmail.com>
2023-06-15 19:19:03 +0530

    [sneha]Added program for OopsConcept

---
+++ Polymorphism.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class Polymorphism
+    {
+
+    }
+    //Static Polymorphism
+    public class StaticPolymorphism
+    {
+        public void Mult(int a, int b)
+        {
+            Console.WriteLine("The multiplication of two number is " + a * b);
+        }
+
+        public void Mult(int a, float b, int c)
+        {
+            Console.WriteLine("The multiplication of three number is " + a * b * c);
+        }
+
+        public void Mult(float a, float b)
+        {
+            Console.WriteLine("The multiplication of two number is " + a * b);
+        }
+    }
+
+    //Dynamic Polymorphism
+    public class CarBrand
+    {
+
+        public virtual void CarSound()
+        {
+            Console.WriteLine("The Car sound tuu tuuu!");
+        }
+    }
+
+    class Hyundai : CarBrand  // Derived class (child) 
+    {
+        public override void CarSound()
+        {
+            Console.WriteLine("The Hyundai Sounds Vrumm Vrumm!");
+        }
+    }
+
+    class BMW : CarBrand  // Derived class (child) 
+    {
+        public override void CarSound()
+        {
+            Console.WriteLine("The BMW Sounds Broom Broom!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome To Oops Concepts Problems");
             Console.WriteLine("Select any one option:- \n1.Object and Class Example." +
-                              "\n2.Inheritance Example.");
+                              "\n2.Inheritance Example."+ "\n3.Polymorphism Example.");
 
             int option = Convert.ToInt32(Console.ReadLine());
 
@@ -21,6 +21,19 @@
                     Car myCar = new Car();
                     myCar.honk();
                     Console.WriteLine(myCar.brand + " " + myCar.modelName);
+                    break;
+                case 3:
+                    StaticPolymorphism polymorphism = new StaticPolymorphism();
+                    polymorphism.Mult(4, 5);
+                    polymorphism.Mult(6, 7.5f, 7);
+                    polymorphism.Mult(3.3f, 6.6f);
+                    Console.WriteLine("-------------------");
+                    CarBrand obj2 = new CarBrand();
+                    obj2.CarSound();
+                    obj2 = new Hyundai();
+                    obj2.CarSound();
+                    obj2 = new BMW();
+                    obj2.CarSound();
                     break;
             }                         
         
