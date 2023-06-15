d232870f2e85f264b2cc5b186570bb406adfd777
Snehasrg <sghongade642@gmail.com>
2023-06-15 18:53:37 +0530

    [sneha]Added program for OopsConcept

---
+++ ObjectClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class ObjectClass
+    {
+        public string Name = "Royal Enfield";
+        public string Colour = "Black";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace OopsConcept
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            ObjectClass obj = new ObjectClass();
+            Console.WriteLine(obj.Name);
+            Console.WriteLine(obj.Colour);
+        }
+    }
+}
